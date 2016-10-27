93407651e2dcfbbdf590f7f79ad3cc4279f04f64
experionashith <ashith.madan@experionglobal.com>
2016-10-27 17:29:23 +0530

    updated

--- fact.java
+++ fact.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;   
 
  class FactorialExample
-{  
+{
      public static void main(String args[])
-	{  
-      int i,fact=1;  
+	{
+      int i,fact=1;
        System.out.println("Please enter number");
-               
+
                         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                        number = Integer.parseInt(br.readLine());
-     
+//                        number = Integer.parseInt(br.readLine());
+                        number = Integer.parseInt(System.console().readLine());
+
       for(i=1;i<=number;i++)
-	{    
-          fact=fact*i;    
-      	}    
-      System.out.println("Factorial of "+number+" is: "+fact);    
-     }  
-}  
+	{
+          fact=fact*i;
+      	}
+      System.out.println("Factorial of "+number+" is: "+fact);
+     }
+}
